ade8697757a48ea23c7d270897a009659c04ef8c
Kartik Rathore <kartikrathore770@gmail.com>
2026-07-07 12:38:14 +0530

    cors issue resolved.

--- backend/src/config/cors.js
+++ backend/src/config/cors.js
@@ -1,8 +1,38 @@
+const allowedOrigins = new Set([
+  "http://localhost:5173",
+  "https://ih.technosthan.com",
+]);
+
+const isAllowedOrigin = (origin) => {
+  if (!origin) {
+    return true;
+  }
+
+  if (allowedOrigins.has(origin)) {
+    return true;
+  }
+
+  return /^https:\/\/([a-z0-9-]+\.)*technosthan\.com$/i.test(origin);
+};
+
 export const corsOptions = {
-  origin: [
-    "http://localhost:5173",
-    "https://ih.technosthan.com",
-  ],
+  origin: (origin, callback) => {
+    if (isAllowedOrigin(origin)) {
+      callback(null, true);
+      return;
+    }
+
+    callback(new Error(`CORS blocked for origin: ${origin}`));
+  },
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization", "x-auth-token", "x-technosthan-token"],
+  allowedHeaders: [
+    "Content-Type",
+    "Authorization",
+    "Accept",
+    "Origin",
+    "X-Requested-With",
+    "x-auth-token",
+    "x-technosthan-token",
+  ],
+  optionsSuccessStatus: 200,
 };
