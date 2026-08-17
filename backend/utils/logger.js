1dd71c41e1a2966db17689565de07e6c0f3e8d16
Technosthan <technosthan@gmail.com>
2026-08-17 09:19:55 +0530

    Delete backend directory

--- backend/utils/logger.js
+++
@@ -1,8 +0,0 @@
-const ActivityLog = require('../models/ActivityLog');
-
-const log = async (userId, action, detail = '') => {
-  try { await ActivityLog.create({ userId, action, detail }); }
-  catch (err) { console.error('Log error:', err.message); }
-};
-
-module.exports = { log };
